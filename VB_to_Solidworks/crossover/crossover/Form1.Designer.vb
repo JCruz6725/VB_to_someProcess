2ead99dbc080bebf7a1462163ff63c0fb21c1e4f
JCruz6725 <35982889+JCruz6725@users.noreply.github.com>
2022-01-26 10:27:11 -0600

    Add files via upload
    Initial code

---
+++ VB_to_Solidworks/crossover/crossover/Form1.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_quit = New System.Windows.Forms.Button()
+        Me.btn_update = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.tbx_float = New System.Windows.Forms.TextBox()
+        Me.lbl_types_title = New System.Windows.Forms.Label()
+        Me.lbl_read_values = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn_quit
+        '
+        Me.btn_quit.Location = New System.Drawing.Point(152, 226)
+        Me.btn_quit.Name = "btn_quit"
+        Me.btn_quit.Size = New System.Drawing.Size(75, 23)
+        Me.btn_quit.TabIndex = 0
+        Me.btn_quit.Text = "Quit"
+        Me.btn_quit.UseVisualStyleBackColor = True
+        '
+        'btn_update
+        '
+        Me.btn_update.Location = New System.Drawing.Point(37, 226)
+        Me.btn_update.Name = "btn_update"
+        Me.btn_update.Size = New System.Drawing.Size(75, 23)
+        Me.btn_update.TabIndex = 2
+        Me.btn_update.Text = "Update"
+        Me.btn_update.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 15)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Bool"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(37, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(21, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Int"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(37, 110)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 15)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Float"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(37, 139)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 15)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "String"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(94, 49)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 7
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(94, 78)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox2.TabIndex = 8
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(94, 136)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox3.TabIndex = 9
+        '
+        'tbx_float
+        '
+        Me.tbx_float.Location = New System.Drawing.Point(94, 107)
+        Me.tbx_float.Name = "tbx_float"
+        Me.tbx_float.Size = New System.Drawing.Size(100, 23)
+        Me.tbx_float.TabIndex = 10
+        '
+        'lbl_types_title
+        '
+        Me.lbl_types_title.AutoSize = True
+        Me.lbl_types_title.Location = New System.Drawing.Point(37, 22)
+        Me.lbl_types_title.Name = "lbl_types_title"
+        Me.lbl_types_title.Size = New System.Drawing.Size(36, 15)
+        Me.lbl_types_title.TabIndex = 11
+        Me.lbl_types_title.Text = "Types"
+        '
+        'lbl_read_values
+        '
+        Me.lbl_read_values.AutoSize = True
+        Me.lbl_read_values.Location = New System.Drawing.Point(94, 22)
+        Me.lbl_read_values.Name = "lbl_read_values"
+        Me.lbl_read_values.Size = New System.Drawing.Size(69, 15)
+        Me.lbl_read_values.TabIndex = 12
+        Me.lbl_read_values.Text = "Read Values"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(311, 305)
+        Me.Controls.Add(Me.lbl_read_values)
+        Me.Controls.Add(Me.lbl_types_title)
+        Me.Controls.Add(Me.tbx_float)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btn_update)
+        Me.Controls.Add(Me.btn_quit)
+        Me.Name = "Form1"
+        Me.Text = "DataGrabber"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_quit As Button
+    Friend WithEvents btn_update As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents tbx_float As TextBox
+    Friend WithEvents lbl_types_title As Label
+    Friend WithEvents lbl_read_values As Label
+End Class
---
+++ VB_to_Solidworks/crossover/crossover/Form1.vb
@@ -0,0 +1,159 @@
+﻿
+
+Public Class Form1
+
+    Private _memManager As New MemoryManager
+
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btn_quit_Click(sender As Object, e As EventArgs) Handles btn_quit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btn_update.Click
+
+        ''' mod this to grab vals from the attached process. 
+
+
+        If _memManager.TryAttachToProcess("FreeCAD") Then
+            Dim _myScore As Double = _memManager.ReadDouble(&H186670981F0)
+            ' Done using MemoryManager for reading. Close it.
+            _memManager.DetachFromProcess()
+            tbx_float.Text = _myScore.ToString()
+
+
+        Else
+            MessageBox.Show("MemoryManager unable to attach")
+        End If
+    End Sub
+
+End Class
+
+
+
+''' <summary>
+''' from https://www.mpgh.net/forum/33-visual-basic-programming/573941-vb-net-readprocessmemory-part1.html
+''' abuckau907
+''' With some minor modification
+''' </summary>
+
+Public Class MemoryManager
+
+    Private Declare Function OpenProcess Lib "kernel32.dll" (ByVal dwDesiredAcess As UInt32, ByVal bInheritHandle As Boolean, ByVal dwProcessId As Int32) As IntPtr
+    Private Declare Function ReadProcessMemory Lib "kernel32" (ByVal hProcess As IntPtr, ByVal lpBaseAddress As IntPtr, ByVal lpBuffer() As Byte, ByVal iSize As Integer, ByRef lpNumberOfBytesRead As Integer) As Boolean
+    Private Declare Function CloseHandle Lib "kernel32.dll" (ByVal hObject As IntPtr) As Boolean
+
+    Private _targetProcess As Process = Nothing 'to keep track of it. not used yet.
+    Private _targetProcessHandle As IntPtr = IntPtr.Zero 'Used for ReadProcessMemory
+    Private PROCESS_ALL_ACCESS As UInt32 = &H1F0FFF
+    Private PROCESS_VM_READ As UInt32 = &H10
+
+
+
+    Public Function TryAttachToProcess(ByVal windowCaption As String) As Boolean
+        Dim _allProcesses() As Process = Process.GetProcesses
+        For Each pp As Process In _allProcesses
+            If pp.MainWindowTitle.ToLower.Contains(windowCaption.ToLower) Then
+                'found it! proceed.
+                Return TryAttachToProcess(pp)
+            End If
+        Next
+        MessageBox.Show("Unable to find process '" & windowCaption & ".' Is running?")
+        Return False
+    End Function
+
+    Public Function TryAttachToProcess(ByVal proc As Process) As Boolean
+        If _targetProcessHandle = IntPtr.Zero Then 'not already attached
+            _targetProcess = proc
+            _targetProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, False, _targetProcess.Id)
+            If _targetProcessHandle = 0 Then
+                TryAttachToProcess = False
+                ''MessageBox.Show("OpenProcess() FAIL! Are you Administrator??")
+            Else
+                'if we get here, all connected and ready to use ReadProcessMemory()
+                TryAttachToProcess = True
+                '''MessageBox.Show("OpenProcess() OK")
+            End If
+        Else
+            '''MessageBox.Show("Already attached! (Please Detach first?)")
+            TryAttachToProcess = False
+        End If
+    End Function
+
+    Public Sub DetachFromProcess()
+        If Not (_targetProcessHandle = IntPtr.Zero) Then
+            _targetProcess = Nothing
+            Try
+                CloseHandle(_targetProcessHandle)
+                _targetProcessHandle = IntPtr.Zero
+                '''MessageBox.Show("MemReader::Detach() OK")
+            Catch ex As Exception
+                '''MessageBox.Show("MemoryManager::DetachFromProcess::CloseHandle error " & Environment.NewLine & ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Public Function ReadInt16(ByVal addr As IntPtr) As Int16
+        Dim _rtnBytes(1) As Byte
+        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 2, vbNull)
+        Return BitConverter.ToInt16(_rtnBytes, 0)
+    End Function
+    Public Function ReadInt32(ByVal addr As IntPtr) As Int32
+        Dim _rtnBytes(3) As Byte
+        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 4, vbNull)
+
+        Return BitConverter.ToInt32(_rtnBytes, 0)
+    End Function
+    Public Function ReadInt64(ByVal addr As IntPtr) As Int64
+        Dim _rtnBytes(7) As Byte
+        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 8, vbNull)
+        Return BitConverter.ToInt64(_rtnBytes, 0)
+    End Function
+    Public Function ReadUInt16(ByVal addr As IntPtr) As UInt16
+        Dim _rtnBytes(1) As Byte
+        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 2, vbNull)
+        Return BitConverter.ToUInt16(_rtnBytes, 0)
+    End Function
+    Public Function ReadUInt32(ByVal addr As IntPtr) As UInt32
+        Dim _rtnBytes(3) As Byte
+        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 4, vbNull)
+        Return BitConverter.ToUInt32(_rtnBytes, 0)
+    End Function
+    Public Function ReadUInt64(ByVal addr As IntPtr) As UInt64
+        Dim _rtnBytes(7) As Byte
+        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 8, vbNull)
+        Return BitConverter.ToUInt64(_rtnBytes, 0)
+    End Function
+    Public Function ReadFloat(ByVal addr As IntPtr) As Single
+        Dim _rtnBytes(3) As Byte
+        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 4, vbNull)
+        Return BitConverter.ToSingle(_rtnBytes, 0)
+    End Function
+    Public Function ReadDouble(ByVal addr As IntPtr) As Double
+        Dim _rtnBytes(7) As Byte
+        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, 8, vbNull)
+        Return BitConverter.ToDouble(_rtnBytes, 0)
+    End Function
+    Public Function ReadIntPtr(ByVal addr As IntPtr) As IntPtr
+        Dim _rtnBytes(IntPtr.Size - 1) As Byte
+        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, IntPtr.Size, Nothing)
+        If IntPtr.Size = 4 Then
+            Return New IntPtr(BitConverter.ToUInt32(_rtnBytes, 0))
+        Else
+            Return New IntPtr(BitConverter.ToInt64(_rtnBytes, 0))
+        End If
+    End Function
+    Public Function ReadBytes(ByVal addr As IntPtr, ByVal size As Int32) As Byte()
+        Dim _rtnBytes(size - 1) As Byte
+        ReadProcessMemory(_targetProcessHandle, addr, _rtnBytes, size, vbNull)
+        Return _rtnBytes
+    End Function
+
+
+End Class
